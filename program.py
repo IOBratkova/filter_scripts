851fba7d5987b1f9c3149566634a6b6e28dc2137
IOBratkova <bratckova.i@yandex.ru>
2020-02-18 11:08:55 +0300

    Upd program.py
    Добавлено описание главного файла программы

--- program.py
+++ program.py
@@ -1,12 +1,12 @@
 from functions import make_case_medical_history, make_case_journal
 from data import fields_section, fields_chronology, fields_journal
-from classes.TestCaseJournalFilter import TestCaseJournalFilter
-# # Создание тест-кейсов для режима "Просмотр по разделам"
-# make_case_medical_history('Просмотр по разделам', fields_section, 'sections', 25)
-#
-# # Создание тест-кейсов для режима "Хронология"
-# make_case_medical_history('Хронология', fields_chronology, 'chronology', 26)
 
-# Создание журнала
+# Создание тест-кейсов для режима "Просмотр по разделам"
+make_case_medical_history('Просмотр по разделам', fields_section, 'sections', 25)
+
+# Создание тест-кейсов для режима "Хронология"
+make_case_medical_history('Хронология', fields_chronology, 'chronology', 26)
+
+# Создание тестов для журнала
 make_case_journal(fields_journal, 'journal', 27)
 
